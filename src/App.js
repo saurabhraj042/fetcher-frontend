3c9a9b849a9e10304eed22349bbf478056daeaf5
saurabhraj042 <saurabhraj042@gmail.com>
2022-04-12 12:03:33 +0530

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
-import React, {Component} from 'react'
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
+import Header from './components/Header';
 import Body from './components/Body';
 import Welcome from './components/Welcome';
 import Footer from './components/Footer';
 
-class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			loading : true,
-			data : []
-		}
-		this.apiCall = this.apiCall.bind(this)
-	}
-	
-	async apiCall(){
-		const url = "https://evening-springs-70151.herokuapp.com/"
-		const resp = await fetch(url)
-		const data = await resp.json()
-		console.log(data)
-		this.setState({
-			loading : false,
-			data : data
-		})
-	}
-	
-	render(){
-		return (
-			<Container fluid>
-				<div class="bg"></div>
-				<div class="bg bg2"></div>
-				<div class="bg bg3"></div>
-				{!this.state.loading && <Header apiCall={this.apiCall} loading={this.state.loading}/>}
-				<div>{this.state.loading && <Welcome />}</div>
-				{this.state.loading && <Header apiCall={this.apiCall} loading={this.state.loading}/>}
-				<Body data={this.state.data}/>
-				<Footer />
-			</Container>
-		)
-	}
+export default function App() {
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState(null);
+
+	const apiCall = async () => {
+		const url = 'https://evening-springs-70151.herokuapp.com/';
+		const resp = await fetch(url);
+		const data = await resp.json();
+		console.log(data);
+		setLoading(false);
+		setData(data);
+	};
+
+	return (
+		<Container fluid>
+			<div class='bg'></div>
+			<div class='bg bg2'></div>
+			<div class='bg bg3'></div>
+			{!loading && <Header apiCall={apiCall} loading={loading} />}
+			<div>{loading && <Welcome />}</div>
+			{loading && <Header apiCall={apiCall} loading={loading} />}
+			<Body data={data} />
+			<Footer />
+		</Container>
+	);
 }
-export default App
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,59 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+import FuzzySearch from 'react-fuzzy';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col, ListGroup } from "react-bootstrap";
-import NoticeItem from "./NoticeItem";
+import { Row, Col, ListGroup } from 'react-bootstrap';
+import NoticeItem from './NoticeItem';
 
-function Body(props){
-    return(
-        <div className="mainBody centered">
-            <Row>
-                <Col>
-                    <ListGroup>
-                        {props.data.map(notice => 
-                                        <NoticeItem 
-                                            key={notice.link} 
-                                            link={notice.link} 
-                                            title={notice.title}
-                                        />
-                                        )
-                        }
-                    </ListGroup>
-                </Col>
-            </Row>
-        </div>
-    )
+function Body(props) {
+	const [data, setData] = useState(null);
+
+	const findItem = (val) => {
+		const data = props?.data;
+
+		data.includes(val) ? setData(val) : setData(null);
+	};
+
+	return (
+		<div className='mainBody centered'>
+			<Row>
+				<Col>
+					<div style={{ width: '100%', marginBottom: '10px' }}>
+						{props?.data ? (
+							<FuzzySearch
+								list={props.data}
+								keys={['title']}
+								width={612}
+								onSelect={(newSelectedItem) => {
+									findItem(newSelectedItem);
+								}}
+								resultsTemplate={(props, state, styles, clickHandler) => {
+									return state.results.map((val, i) => {
+										const style =
+											state.selectedIndex === i ? styles.selectedResultStyle : styles.resultsStyle;
+										return (
+											<div key={i} style={style} onClick={() => clickHandler(i)}>
+												{val.title}
+												<span style={{ float: 'right', opacity: 0.5 }}>by {val.author}</span>
+											</div>
+										);
+									});
+								}}
+							/>
+						) : null}
+					</div>
+					<ListGroup>
+						{props?.data && data === null ? (
+							props?.data?.map((notice) => (
+								<NoticeItem key={notice.link} link={notice.link} title={notice.title} />
+							))
+						) : data != null ? (
+							<NoticeItem key={data.link} link={data.link} title={data.title} />
+						) : null}
+					</ListGroup>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
-export default Body
+export default Body;
